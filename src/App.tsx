7928a649f369b6b975deb72e580dd37c9ddde00b
Lassikainen <lobb.richard@gmail.com>
2020-11-21 21:15:56 +1300

    feat: Import Header into App and conditionally show pages
    Import Header into App.tsx and conditionally show GeneratorPage or SavePage depending on state
    Import headerReducer into rootReducer and combine

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import "./App.css";
-import GeneratorPage from './components/generator/GeneratorPage'
-import SavePage from './components/saved/SavePage'
+import GeneratorPage from "./components/generator/GeneratorPage";
+import SavePage from "./components/saved/SavePage";
+import Header from "./components/header/Header";
+
+import { useSelector } from "react-redux";
 
 function App() {
+  const currentPage = useSelector(
+    (state: any) => state.headerReducer.currentPage
+  );
+
   return (
     <div className="App">
       <h1> PIN GENERATOR APP</h1>
-
-      <GeneratorPage/>
-      <SavePage/>
+      <div className="outer-container">
+        <Header />
+        {currentPage === "generate" ? <GeneratorPage /> : null}
+        {currentPage === "saved" ? <SavePage /> : null}
+      </div>
     </div>
   );
 }
--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,7 +1,12 @@
 import { combineReducers } from "redux";
 import { generatorReducer } from "../components/generator/reducer";
 import { savedReducer } from "../components/saved/reducer";
+import { headerReducer } from "../components/header/reducer";
 
-const rootReducer = combineReducers({ generatorReducer, savedReducer });
+const rootReducer = combineReducers({
+  generatorReducer,
+  savedReducer,
+  headerReducer,
+});
 
 export default rootReducer;
