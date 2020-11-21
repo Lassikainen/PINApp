77ec7981ab2ce4598f9c18383b333475942bcbd1
Lassikainen <lobb.richard@gmail.com>
2020-11-21 16:34:41 +1300

    feat: Import generatorPage to App
    Import generatorPage to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import GeneratorPage from './components/generator/GeneratorPage'
 
 function App() {
   return (
     <div className="App">
+      <h1> PIN GENERATOR APP</h1>
+
+      <GeneratorPage/>
     </div>
   );
 }
