b01dcb86bc75b3b6e9861a26f752afce4041f317
Lassikainen <lobb.richard@gmail.com>
2020-11-21 22:53:01 +1300

    test: Add RTL test suites
    Add RTL test suites for App, GeneratorPage, Header and SavePage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+describe("Renders starting components", () => {
+  test("renders App component", () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  });
 });
