668a0d494740f91f1bfdf1bdb161b4cdbebb9821
Lassikainen <lobb.richard@gmail.com>
2020-11-21 15:30:42 +1300

    feat: Add Redux action constants
    Add Redux action constants for generating, saving, updating and deleting PINs
    Add Redux action constants for changing page

--- src/redux/actionConstants.ts
+++ src/redux/actionConstants.ts
@@ -1,1 +1,6 @@
-export const PLACEHOLDER_ACTION = 'placeholder_action'
+export const GENERATE_NEW_PIN = "generate_new_pin";
+export const SAVE_PIN = "save_pin";
+export const UPDATE_PIN_NAME = "update_pin_name";
+export const DELETE_PIN = "delete_pin";
+export const SHOW_GENERATOR_PAGE = "show_generator_page";
+export const SHOW_SAVED_PAGE = "show_saved_page";
